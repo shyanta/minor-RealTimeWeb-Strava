1f3a53f52eab43d147509a323150e6f59fa0137f
Shyanta Vleugel <Shyanta Vleugel>
2017-05-03 11:17:09 +0200

    Add response to oauth route

--- routes/oauth.js
+++ routes/oauth.js
@@ -5,39 +5,18 @@ var router = express.Router();
 
 router.get('*', function(req, res){
 	var code = req.query.code;
-	// console.log(code);
 	var postUrl = 'https://www.strava.com/oauth/token';
-	console.log('post', {
-		client_id: process.env.CLIENT_ID,
-		client_secret: process.env.CLIENT_SECRET,
-		code: code
-	})
-	//
-	// request.post(postUrl,{
-	// 	client_id: process.env.CLIENT_ID,
-	// 	client_secret: process.env.CLIENT_SECRET,
-	// 	code: code
-	// }, function (err, result, body){
-	// 	console.log(err, result, body);
-	// })
-	//
-	// request.post(postUrl, {form:{
-	// 	client_id: process.env.CLIENT_ID,
-	// 	client_secret: process.env.CLIENT_SECRET,
-	// 	code: code
-	// }}, function (err, result, body){
-	// 	console.log(err, result, body);
-	// })
+	var data;
 
 	request.post({url:postUrl, form: {
 		client_id: process.env.CLIENT_ID,
 		client_secret: process.env.CLIENT_SECRET,
 		code: code
 	}}, function(err,httpResponse,body){
-		console.log(body)
-		res.redirect('main', body);
+		data = body
 	})
-	res.render('oauth');
+	res.render('main');
+	res.redirect('/main', body);
 });
 
 module.exports = router;
