61d470ef1399f895c3765826e8594709e715bb12
Shyanta Vleugel <Shyanta Vleugel>
2017-05-03 10:36:52 +0200

    Rewrite request.post function

--- routes/oauth.js
+++ routes/oauth.js
@@ -7,14 +7,34 @@ router.get('*', function(req, res){
 	var code = req.query.code;
 	// console.log(code);
 	var postUrl = 'https://www.strava.com/oauth/token/';
-	console.log('post')
-
-	request.post(postUrl, {
+	console.log('post', {
 		client_id: process.env.CLIENT_ID,
 		client_secret: process.env.CLIENT_SECRET,
 		code: code
-	}, function (err, result, body){
-		console.log(err, result, body);
+	})
+	//
+	// request.post(postUrl,{
+	// 	client_id: process.env.CLIENT_ID,
+	// 	client_secret: process.env.CLIENT_SECRET,
+	// 	code: code
+	// }, function (err, result, body){
+	// 	console.log(err, result, body);
+	// })
+	//
+	// request.post(postUrl, {form:{
+	// 	client_id: process.env.CLIENT_ID,
+	// 	client_secret: process.env.CLIENT_SECRET,
+	// 	code: code
+	// }}, function (err, result, body){
+	// 	console.log(err, result, body);
+	// })
+
+	request.post({url:postUrl, form: {
+		client_id: process.env.CLIENT_ID,
+		client_secret: process.env.CLIENT_SECRET,
+		code: code
+	}}, function(err,httpResponse,body){
+		console.log(err,httpResponse,body)
 	})
 
 	res.render('oauth');
