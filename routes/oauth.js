eb9b0c5ee483921c7443d926f5614b2d33b3a969
Shyanta Vleugel <Shyanta Vleugel>
2017-05-03 19:39:28 +0200

    Save data directly to app

--- routes/oauth.js
+++ routes/oauth.js
@@ -6,18 +6,15 @@ var router = express.Router();
 router.get('*', function(req, res){
 	var code = req.query.code;
 	var postUrl = 'https://www.strava.com/oauth/token';
-	var data;
-
 
 	request.post({url:postUrl, form: {
 		client_id: process.env.CLIENT_ID,
 		client_secret: process.env.CLIENT_SECRET,
 		code: code
 	}}, function(err,httpResponse,body){
-		data = body;
-		// res.redirect('/main', {data});
+		req.app.set('tokenData', {data});
 	})
-	req.app.set('tokenData', {data});
+
 	res.render('oauth');
 });
 
