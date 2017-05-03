05cb1b17a8e84b407cb891c34c98801cca4d1e43
Shyanta Vleugel <Shyanta Vleugel>
2017-05-03 19:41:46 +0200

    Change 'data' to 'body'"

--- routes/oauth.js
+++ routes/oauth.js
@@ -12,7 +12,7 @@ router.get('*', function(req, res){
 		client_secret: process.env.CLIENT_SECRET,
 		code: code
 	}}, function(err,httpResponse,body){
-		req.app.set('tokenData', {data});
+		req.app.set('tokenData', {body});
 	})
 
 	res.render('oauth');
