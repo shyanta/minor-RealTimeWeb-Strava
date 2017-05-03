33d5ff6e679470e0e1d723cbe96dce00195524f3
Shyanta Vleugel <Shyanta Vleugel>
2017-05-03 09:53:00 +0200

    Test first step oauth

--- routes/oauth.js
+++ routes/oauth.js
@@ -3,17 +3,16 @@ var express = require('express');
 var router = express.Router();
 
 router.get('*', function(req, res){
-	var code = req.query.code;
-	var postUrl = 'https://www.strava.com/oauth/token/';
-
-	request.post(postUrl, {
-		client_id: process.env.CLIENT_ID,
-		client_secret: process.env.CLIENT_SECRET,
-		code: code
-	})
+	// var code = req.query.code;
+	// var postUrl = 'https://www.strava.com/oauth/token/';
+	//
+	// request.post(postUrl, {
+	// 	client_id: process.env.CLIENT_ID,
+	// 	client_secret: process.env.CLIENT_SECRET,
+	// 	code: code
+	// })
 
 	res.render('oauth');
-
 });
 
 module.exports = router;
