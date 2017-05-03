f2fafeef09e0d80f75b557ce69b9144fc5f2a350
Shyanta Vleugel <Shyanta Vleugel>
2017-05-03 23:26:39 +0200

    API Call Current Athlete and print data

--- routes/main.js
+++ routes/main.js
@@ -1,12 +1,26 @@
 var express = require('express');
+var request = require('request');
 
 var router = express.Router();
 
 router.get('/', function(req, res){
 	var data = req.app.get('tokenData');
-	console.log(data);
+	res.locals.access = data;
 
 	res.render('main');
+	var access_token = data.access_token;
+	console.log(access_token + ' token');
+
+	var apiCurrentAthlete = 'https://www.strava.com/api/v3/athlete';
+
+	request({
+		url: apiCurrentAthlete,
+		headers: {
+		'Bearer': 'request'
+	}, function(err, response, body){
+		res.locals.currentAthlete = body;
+		console.log(body);
+	});
 });
 
 module.exports = router;
