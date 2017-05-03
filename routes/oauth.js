7274b532ee3b1f18d178e3423b2b73701a7ea318
Shyanta Vleugel <Shyanta Vleugel>
2017-05-03 10:10:58 +0200

    Add request.post to oauth router

--- routes/oauth.js
+++ routes/oauth.js
@@ -4,14 +4,14 @@ var router = express.Router();
 
 router.get('*', function(req, res){
 	var code = req.query.code;
-	console.log(code);
-	// var postUrl = 'https://www.strava.com/oauth/token/';
-	//
-	// request.post(postUrl, {
-	// 	client_id: process.env.CLIENT_ID,
-	// 	client_secret: process.env.CLIENT_SECRET,
-	// 	code: code
-	// })
+	// console.log(code);
+	var postUrl = 'https://www.strava.com/oauth/token/';
+
+	request.post(postUrl, {
+		client_id: process.env.CLIENT_ID,
+		client_secret: process.env.CLIENT_SECRET,
+		code: code
+	})
 
 	res.render('oauth');
 });
