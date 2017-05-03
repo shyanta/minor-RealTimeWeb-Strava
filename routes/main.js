fe4eb97ec4ee1399e48666fd166447a967506467
Shyanta Vleugel <Shyanta Vleugel>
2017-05-03 09:44:28 +0200

    Move post request to oauth route

--- routes/main.js
+++ routes/main.js
@@ -4,14 +4,7 @@ var request = require('request');
 var router = express.Router();
 
 router.get('/', function(req, res){
-	var code = req.app.get('code');
-	var postUrl = 'https://www.strava.com/oauth/token/';
 
-	request.post(postUrl, {
-		client_id: process.env.CLIENT_ID,
-		client_secret: process.env.CLIENT_SECRET,
-		code: code
-	})
 	res.render('main');
 });
 
--- routes/oauth.js
+++ routes/oauth.js
@@ -4,7 +4,14 @@ var router = express.Router();
 
 router.get('*', function(req, res){
 	var code = req.query.code;
-	req.app.set('code', code);
+	var postUrl = 'https://www.strava.com/oauth/token/';
+
+	request.post(postUrl, {
+		client_id: process.env.CLIENT_ID,
+		client_secret: process.env.CLIENT_SECRET,
+		code: code
+	})
+
 	res.render('oauth');
 
 });
