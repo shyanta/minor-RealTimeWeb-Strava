b6d58147f09912ab3b484f4811e3f96645643b4a
Shyanta Vleugel <Shyanta Vleugel>
2017-05-03 10:24:37 +0200

    Add debugging console logs

--- routes/oauth.js
+++ routes/oauth.js
@@ -7,11 +7,14 @@ router.get('*', function(req, res){
 	var code = req.query.code;
 	// console.log(code);
 	var postUrl = 'https://www.strava.com/oauth/token/';
+	console.log('post')
 
 	request.post(postUrl, {
 		client_id: process.env.CLIENT_ID,
 		client_secret: process.env.CLIENT_SECRET,
 		code: code
+	}, function (err, result, body){
+		console.log(err, result, body);
 	})
 
 	res.render('oauth');
